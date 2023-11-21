dbf4532287d1f1050e11ef9bb69401f04ac9c536
Diego Gomez <gomedie1974@hotmail.com>
2023-11-21 11:53:04 -0300

    Proyecto creado, versión final

--- sistema_mito/urls.py
+++ sistema_mito/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from sistema_mito.views import comienzo
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('comienzo/', comienzo)
 ]
---
+++ sistema_mito/views.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def comienzo(request):
+    pass
