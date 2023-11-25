3c1c9ce2c0357fafbc569a1f6d63c7fc96d6fb67
Diego Gomez <gomedie1974@hotmail.com>
2023-11-25 20:53:18 -0300

    Proyecto creado, versión final, arreglando chat

--- mensajeria/models.py
+++ mensajeria/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Chat(models.Model):
     usuario_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats_usuario_1')
     usuario_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats_usuario_2')
@@ -14,6 +15,7 @@ class Message(models.Model):
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     texto = models.TextField()
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-
+ 
     def __str__(self):
         return f"{self.fecha_creacion} ({self.texto}) ({self.chat})"
+  
--- mensajeria/views.py
+++ mensajeria/views.py
@@ -2,11 +2,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Chat
 
+
 def chat_detail(request, chat_id):
     chat = Chat.objects.get(id=chat_id)
     messages = chat.message_set.order_by('fecha_creacion')
     return render(request, 'mensajeria/chat_detail.html', {'chat': chat, 'messages': messages})
- 
+    
 def chat_list(request):
     chats = Chat.objects.all()
     return render(request, 'mensajeria/chat_list.html', {'chats': chats})
