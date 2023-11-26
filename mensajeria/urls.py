236920799b0bec3299bde950a52753f5ab7ed7a8
Diego Gomez <gomedie1974@hotmail.com>
2023-11-26 16:11:44 -0300

    Proyecto creado, versión final, arreglando detalles

--- mensajeria/urls.py
+++ mensajeria/urls.py
@@ -5,4 +5,6 @@ from .views import chat_list, chat_detail
 urlpatterns = [
     path('chats/', chat_list, name='chat_list'),
     path('chat/<int:chat_id>/', chat_detail, name='chat_detail'),
+
+    
 ]
